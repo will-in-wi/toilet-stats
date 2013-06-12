9fb93b9e68695a769e690360f29667d7a49b02a5
William Johnston <will@johnstonclan.net>
2013-06-12 09:31:42 -0500

    Abstract code into methods.

--- toilet.php
+++ toilet.php
@@ -19,9 +19,11 @@ class Toilet
 			return false;
 		}
 
+		$this->add_card();
+
 		// Add cards until the deck has at least 4.
 		while (count($this->working_deck) < 4) {
-			$this->working_deck[] = array_shift($this->unused_cards);
+			$this->add_card();
 		}
 
 		// TODO: stick the stuff below in a loop.
@@ -33,10 +35,20 @@ class Toilet
 		// Check for same number four cards back.
 		if ($top_card->get_number() === $fourth_card->get_number()) {
 			// Pull the last four cards and discard them.
-			unset($this->working_deck[$last_index]);
-			unset($this->working_deck[$last_index-1]);
-			unset($this->working_deck[$last_index-2]);
-			unset($this->working_deck[$last_index-3]);
+			$this->discard_card($last_index);
+			$this->discard_card($last_index-1);
+			$this->discard_card($last_index-2);
+			$this->discard_card($last_index-3);
 		}
+	}
+
+	protected function add_card()
+	{
+		$this->working_deck[] = array_shift($this->unused_cards);
+	}
+
+	protected function discard_card($i)
+	{
+		unset($this->working_deck[$i]);
 	}
 }
