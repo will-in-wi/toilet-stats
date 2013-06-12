88f357402da4a848e1d64682bad0f42dc5583e9f
William Johnston <will@johnstonclan.net>
2013-06-12 08:33:06 -0500

    First logic.

--- toilet.php
+++ toilet.php
@@ -15,10 +15,26 @@ class Toilet
 
 	public function round()
 	{
+		// TODO: Check for 0 unused cards
+
 		// Add cards until the deck has at least 4.
 		while (count($this->working_deck) < 4) {
 			$this->working_deck[] = array_shift($this->unused_cards);
 		}
 
+		// TODO: stick the stuff below in a loop.
+
+		$last_index = count($this->working_deck) - 1;
+		$top_card = $this->working_deck[$last_index];
+		$fourth_card = $this->working_deck[$last_index - 3];
+
+		// Check for same number four cards back.
+		if ($top_card->get_number() === $fourth_card->get_number()) {
+			// Pull the last four cards and discard them.
+			unset($this->working_deck[$last_index]);
+			unset($this->working_deck[$last_index-1]);
+			unset($this->working_deck[$last_index-2]);
+			unset($this->working_deck[$last_index-3]);
+		}
 	}
 }
