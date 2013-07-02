7d138ccd7ab6fb8e9022ab0e48000ad8e3620a65
William Johnston <will@johnstonclan.net>
2013-07-02 10:59:18 -0500

    Clean up arbitrary number of decks.

--- stats.php
+++ stats.php
@@ -8,29 +8,21 @@ $loader = new UniversalClassLoader();
 $loader->useIncludePath(true);
 $loader->register();
 
-function play_game()
+function play_game($decks = 1)
 {
 	$deck = array();
-	for ($i=0; $i < 1; $i++) { 
+	for ($i=0; $i < $decks; $i++) { 
 		$deck1 = \ToiletStats\Cards\Deck::generate();
 		$deck = array_merge($deck, $deck1);
 	}
-	// $deck1 = \ToiletStats\Cards\Deck::generate();
-	// $deck2 = \ToiletStats\Cards\Deck::generate();
-
-	// $deck = array_merge($deck1, $deck2);
-
-	var_dump(count($deck));
-	// exit();
-
-	// Shuffle deck
+	
+	// Shuffle deck(s)
 	shuffle($deck);
 
 	$game = new \ToiletStats\Logic\Toilet($deck);
 
 	$playing = true;
 	while ($playing === true) {
-		// echo "Play round!\n";
 		$playing = $game->round();
 
 		// Deck::format($game->get_deck());
